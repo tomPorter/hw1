47551c44551ccffa99bdc53c936bb2dead7d3b9d
Richard Xia <rxia@eecs.berkeley.edu>
2012-08-31 15:10:47 -0700

    Added sanity specs, skeletons, and README

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ hw1
 ===
 
 Skeleton code and sanity specs for homework 1
+
+Skeleton Code
+=============
+
+The skeleton code for parts 1-7 are located in `skeleton_code/`.
+
+Running Sanity Specs
+====================
+
+To run the sanity specs, please make sure `rspec` is installed.
+To install rspec, run `gem install rspec`.
+If you have your solution saved to a file located at `path/to/solution.rb`, and
+the corresponding spec is located at `other_path/to/sanity_spec.rb`, then you can run
+the sanity spec with:
+
+    rspec path/to/solution.rb other_path/to/sanity_spec.rb
+
+The output should look something like:
+
+    ...
+
+    Finished in 0.00206 seconds
+    3 examples, 0 failures
---
+++ sanity_specs/part4_spec.rb
@@ -0,0 +1,27 @@
+describe "dessert" do
+  it "should have an initialize method" do
+    lambda { Dessert.new }.should_not raise_error(::NoMethodError)
+  end
+
+  it "should have a healthy? method" do
+    lambda { Dessert.new.('a', 1).healthy? }.should_not raise_error(::NoMethodError)
+  end
+
+  it "should have a delicious? method" do
+    lambda { Dessert.new.('a', 1).delicious? }.should_not raise_error(::NoMethodError)
+  end
+end
+
+describe "jellybean" do
+  it "should have an initialize method" do
+    lambda { Dessert.new }.should_not raise_error(::NoMethodError)
+  end
+
+  it "should have a healthy? method" do
+    lambda { Dessert.new.('a', 1).healthy? }.should_not raise_error(::NoMethodError)
+  end
+
+  it "should have a delicious? method" do
+    lambda { Dessert.new.('a', 1).delicious? }.should_not raise_error(::NoMethodError)
+  end
+end
