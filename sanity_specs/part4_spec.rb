f97fc8f3c22d710ab603069b5037f1e7029b4675
Maxwell Feldman <maxwell.feldman@dhcp-130-99.eecs.berkeley.edu>
2012-10-19 15:35:19 -0700

    DRYd jellybean/dessert sanity spec

--- sanity_specs/part4_spec.rb
+++ sanity_specs/part4_spec.rb
@@ -1,27 +1,13 @@
 describe "dessert" do
-  it "should have an initialize method" do
-    lambda { Dessert.new }.should_not raise_error(::NoMethodError)
-  end
-
-  it "should have a healthy? method" do
-    lambda { Dessert.new('a', 1).healthy? }.should_not raise_error(::NoMethodError)
-  end
-
-  it "should have a delicious? method" do
-    lambda { Dessert.new('a', 1).delicious? }.should_not raise_error(::NoMethodError)
+  it "should define a constructor" { lambda { Dessert.new }.should_not raise_error(::NoMethodError) }
+  %w(healthy? delicious? lowfat?).each do |method|
+    it "should define #{method}" { Dessert.new('a',1).should respond_to method }
   end
 end
 
 describe "jellybean" do
-  it "should have an initialize method" do
-    lambda { Dessert.new }.should_not raise_error(::NoMethodError)
-  end
-
-  it "should have a healthy? method" do
-    lambda { Dessert.new('a', 1).healthy? }.should_not raise_error(::NoMethodError)
-  end
-
-  it "should have a delicious? method" do
-    lambda { Dessert.new('a', 1).delicious? }.should_not raise_error(::NoMethodError)
+  it "should define a constructor" { lambda { JellyBean.new }.should_not raise_error(::NoMethodError) }
+  %w(healthy? delicious? lowfat?).each do |method|
+    it "should define #{method}" { JellyBean.new('a',1).should respond_to method }
   end
 end
